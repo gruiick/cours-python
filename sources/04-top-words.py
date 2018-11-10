6da3501259cbe7953d42e32f739277bc1bf130e0
Dimitri Merejkowsky <d.merej@gmail.com>
2018-11-10 16:26:17 +0100

    Session 2: démo

---
+++ sources/04-top-words.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def get_value(pair):
+    key, value = pair
+    return value
+
+
+def get_word(chunk):
+    if all(x.isalpha() for x in chunk):
+        if len(chunk) < 4:
+            return None
+        return chunk.lower()
+    else:
+        return None
+
+
+def main():
+    filename = sys.argv[1]
+    stream = open(filename, "r")
+
+    scores = {}
+
+    for line in stream.readlines():
+        for chunk in line.split():
+            word = get_word(chunk)
+            if word:
+                if not word in scores:
+                    scores[word] = 0
+                else:
+                    scores[word] += 1
+
+    stream.close()
+    top = sorted(scores.items(), key=get_value, reverse=True)
+    for word, count in top[0:10]:
+        print(count, word)
+
+
+if __name__ == "__main__":
+    main()
