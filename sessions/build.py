7c7eee207ad56663315eeab99ac276b4ad8a1131
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-12-03 18:20:58 +0100

    Rewrite build script in Python
    Just becuse

---
+++ sessions/build.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import subprocess
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Usage: build.py NUMBER")
+
+    number = sys.argv[1]
+
+    os.makedirs("build", exist_ok=True)
+    cmd = []
+    if sys.platform == "darwin":
+        # From mactex
+        cmd.append("texi2pdf")
+    else:
+        cmd.append("pdflatex")
+    cmd.append(f"../python-{number}")
+    subprocess.run(cmd, cwd="build", check=True)
+
+
+if __name__ == "__main__":
+    main()
