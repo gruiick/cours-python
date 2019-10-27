d234d76443496ce29aa64309c5a2a29fa16919fd
Dimitri Merejkowsky <d.merej@gmail.com>
2019-10-27 16:02:25 +0100

    Nettoyage du sapin :)

--- saison-2/sources/sapin/sapin_b.py
+++ saison-2/sources/sapin/sapin_b.py
@@ -1,4 +1,3 @@
-
 def largeur():
     n = int(input("donnez une larguer impaire = "))
     return n
@@ -6,29 +5,23 @@ def largeur():
 
 def affiche_sapin(largeur_totale):
     index = int((largeur_totale - 1) / 2)
-    print("index=",index)
-    #print("range(index)=",range(index))
     for index_courant in range(index + 1):
-        #print("index_courant=",index_courant)
-        largeur_courante = (index_courant * 2) +1
-        affiche_ligne(index_courant, largeur_courante,largeur_totale)
+        largeur_courante = (index_courant * 2) + 1
+        affiche_ligne(index_courant, largeur_courante, largeur_totale)
     largeur_pied = int(largeur_totale / 3)
-    affiche_ligne(0, 1,largeur_totale)
-    affiche_ligne(0, 1,largeur_totale)
+    affiche_ligne(0, 1, largeur_totale)
+    affiche_ligne(0, 1, largeur_totale)
 
-def affiche_ligne(index_courant, largeur_courante,largeur_totale):
-    nbr_blanc = int(largeur_totale/2) - index_courant
-    #print("nbr_blanc = ",nbr_blanc,sep = "")
-    blanc = " "*nbr_blanc
-    #print("blanc = a",blanc,"a")
-    print(blanc, "#"*largeur_courante, blanc, sep = "")
-    
 
+def affiche_ligne(index_courant, largeur_courante, largeur_totale):
+    nbr_blanc = int(largeur_totale / 2) - index_courant
+    blanc = " " * nbr_blanc
+    print(blanc, "#" * largeur_courante, blanc, sep="")
 
 
 def main():
     largeur_totale = largeur()
-    affiche_sapin(largeur_totale)  
+    affiche_sapin(largeur_totale)
 
 
 main()
