de6a21cafdbe61c01c998955d323338336283d93
Dimitri Merejkowsky <d.merej@gmail.com>
2019-11-08 20:15:42 +0100

    Génération des pdf dans build/

--- cours/build.py
+++ cours/build.py
@@ -7,7 +7,7 @@ def main():
     template = Path("build.in.ninja").text()
     to_write = template
     for md_file in Path(".").files("*.md"):
-        to_write += f"build  ../{md_file.with_suffix('.pdf')}: pandoc ../{md_file}\n"
+        to_write += f"build {md_file.with_suffix('.pdf')}: pandoc ../{md_file}\n"
     build_path = Path("build")
     build_path.mkdir_p()
     out = build_path / "build.ninja"
