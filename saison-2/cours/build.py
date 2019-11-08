d80a04413b61bb1fbdc91bf0a51d43c1fe1224ed
Dimitri Merejkowsky <d.merej@gmail.com>
2019-11-08 19:34:00 +0100

    Small build system to build lectures

---
+++ saison-2/cours/build.py
@@ -0,0 +1,21 @@
+from path import Path
+import sys
+import subprocess
+
+
+def main():
+    template = Path("build.in.ninja").text()
+    to_write = template
+    for md_file in Path(".").files("*.md"):
+        to_write += f"build  ../{md_file.with_suffix('.pdf')}: pandoc ../{md_file}\n"
+    build_path = Path("build")
+    build_path.mkdir_p()
+    out = build_path / "build.ninja"
+    out.write_text(to_write)
+    process = subprocess.run(["ninja", "-C", build_path])
+    if process.returncode != 0:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
