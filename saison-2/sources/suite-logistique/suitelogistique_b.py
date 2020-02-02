376c2757a74a8c63edc7ceac8b5bc9878fecbfba
Dimitri Merejkowsky <d.merej@gmail.com>
2020-02-02 14:19:45 +0100

    Format with black

--- saison-2/sources/suite-logistique/suitelogistique_b.py
+++ saison-2/sources/suite-logistique/suitelogistique_b.py
@@ -4,12 +4,15 @@ import sys
 
 k = float(sys.argv[1])
 
+
 def f(x):
-    return k * x * (1-x)
+    return k * x * (1 - x)
+
 
 x0 = 0.5
 
 n = 200
+
 
 def liste(n):
     resultat = [x0]
@@ -18,7 +21,7 @@ def liste(n):
     return resultat
 
 
-abscisses = list(numpy.linspace(0, 10, n+1))
+abscisses = list(numpy.linspace(0, 10, n + 1))
 ordonnées = liste(n)
 
 pyplot.plot(abscisses, ordonnées)
