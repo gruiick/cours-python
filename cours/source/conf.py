503247e141c58c6733378787569cf0d47b70b08d
Dimitri Merejkowsky <d.merej@gmail.com>
2020-05-23 14:21:40 +0200

    Utilise le thème agogo
    Il est un peu moins joli mais:
    
    * il est présent par défaut dans sphinx - donc il sera maintenu
    * il y a des jolis liens
    * tout est traduit

--- cours/source/conf.py
+++ cours/source/conf.py
@@ -1,6 +1,3 @@
-import sphinx_nameko_theme
-
-
 project = "Cours Python"
 copyright = "2020, Dimitri Merejkowsky"
 author = "Dimitri Merejkowsky"
@@ -14,6 +11,5 @@ exclude_patterns = []
 keep_warnings = True
 
 html_show_sourcelink = False
-html_theme_path = [sphinx_nameko_theme.get_html_theme_path()]
-html_theme = "nameko"
+html_theme = "agogo"
 html_static_path = ["_static"]
