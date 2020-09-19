7423f0b361d7a1c6e21781fdde9a70dfb2d6a855
Dimitri Merejkowsky <d.merej@gmail.com>
2020-09-19 16:07:08 +0200

    Ajout de l'extension "page not found"

--- cours/source/conf.py
+++ cours/source/conf.py
@@ -2,13 +2,14 @@ project = "Programmation en Python"
 copyright = "2020, Dimitri Merejkowsky"
 author = "Dimitri Merejkowsky"
 
-
 version = "0.3"
 language = "fr"
 
 templates_path = ["_templates"]
 exclude_patterns = []
 keep_warnings = True
+
+extensions = ["notfound.extension"]
 
 html_show_sourcelink = False
 html_theme = "agogo"
