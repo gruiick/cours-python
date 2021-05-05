eec3167d956e4d4226ef5040f297de2de9c60328
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-05 15:39:19 +0200

    Remove files specific to E2L

--- saison-2/sources/refactorings/02_renommage_parametre.py
+++
@@ -1,9 +0,0 @@
-# Exercice: remplacer le paramètre prénom
-# par appellation
-def dire_bonjour(prénom):
-    print("Bonjour " + prénom)
-
-
-dire_bonjour("Alice")
-prénom = "Bob"
-dire_bonjour(prénom)
